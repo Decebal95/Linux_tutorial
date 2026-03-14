4c885d949ca49485f48c5a838a61e3bfd9d847b1
Decebal-Andrei Iordache <iordache.decebal04@gmail.com>
2026-03-14 13:23:40 +0000

    Am adaugat solutii pentru exercitiul 1

---
+++ solutii/ex1_1.sh
@@ -0,0 +1,8 @@
+E1 USOR , ex1_1.sh - Explorare si navigare sistem de fisiere:
+1. pwd
+2. ls -lha demo/
+3. cd -navigare, pwd - calea curenta
+4. ls -l demo/
+5. tree -L 2 ~/Linux_tutorial/
+6. cd demo/ && cd -
+7. ls -lhS demo/
